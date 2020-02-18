3830a31faa143e49df18933222ef0ff2a609a17c
Bug38 <Bug38@users.noreply.github.com>
2020-02-18 23:54:49 +0100

    health and npc
    Health system
    Respawn
    NPC

---
+++ Assets/Health.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour
+{
+    public const int maxHealth = 100;
+    public bool destroyOnDeath;
+
+    [SyncVar(hook = "onChangeHealth")]
+    public int currentHealth = maxHealth;
+
+    public RectTransform healthBar;
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+        currentHealth -= amount;
+
+        if (currentHealth <= 0)
+        {
+            if (destroyOnDeath)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                currentHealth = maxHealth;
+                RpcRespawn();
+            }
+        }
+    }
+
+    void onChangeHealth(int health)
+    { 
+        healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            transform.position = Vector3.zero;
+        }
+    }
+}
